d68d5fa0e3952cee946cc8de3f181fdd17d54809
Jordi Vendrell <jordi.vendrell@marsbased.com>
2017-03-06 13:27:41 +0100

    Implementation of the cookies alert

--- source/javascripts/body.js
+++ source/javascripts/body.js
@@ -1,8 +1,8 @@
 //= require jquery/dist/jquery
 //= require what-input/dist/what-input.min
-
+//= require js-cookie
+//= require cookies
 //= require foundation_requires
-
 //= require appendAround.js
 //= require svg4everybody.min.js
 //= require progressFixed.js
